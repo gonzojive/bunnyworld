a5c31494bd3496dabc34bccfd00977bdd5d748f6
alex.blackstock <alex.blackstock@4b404feb-bb2a-0410-b30c-150bbe6f5b23>
2007-03-03 10:41:11 +0000

    The model should be good to go - Val, as soon as the resource manager is done, we'll have to collaborate on integration. It's important that anything which needs to hear about changes to the model implements one or more of the three different kinds of listeners - this is the only way my classes know to communicate change data. You can just call save() on the document and it works.  Handles GUI, XML, and what not.  There are two different open functions. One happens when the Ctor is called (user chooses file). The other happens when a path is passed into the Ctor (command line style) The catalog is still in progress, but comming along.
    Cheers!
    Alex

---
+++ trunk/Document.java
@@ -0,0 +1,170 @@
+import java.beans.*;
+import java.io.*;
+import java.util.*;
+import javax.swing.JFileChooser;
+
+
+public class Document{
+	//private ResourceCollection rc;
+	private ArrayList<Page> pages;
+	private ArrayList<DocumentListener> listeners;
+	
+	/**
+	 * Default Ctor - use it whenever
+	 */
+	public Document(){
+		pages = new ArrayList<Page>();
+		listeners = new ArrayList<DocumentListener>();
+		load();
+	}
+	
+	/**
+	 * Ctor called if there's a file passed in at the command line
+	 */
+	public Document(String path){
+		pages = new ArrayList<Page>();
+		listeners = new ArrayList<DocumentListener>();
+		load(path);
+	}
+	
+	public void addPage(Page p){
+		pages.add(p);
+		fireChanged(p.getName(), false);
+	}
+	
+	public void deletePage(Page p){
+		pages.remove(p);
+		fireChanged(p.getName(), true);
+	}
+	
+	public void addResource(){
+		JFileChooser choose = new JFileChooser();
+		choose.showOpenDialog(null);
+		File file = null;
+		file = choose.getSelectedFile();
+		if(file != null) /*rc.addResource(file)*/;
+		//fireChanged(rc.getName(), false);
+	}
+	
+	public void deleteResource(){
+		//ask val about this one
+	}
+	
+	public ArrayList<Page> getPages(){
+		return pages;
+	}
+	
+	public Page getPage(String name){
+		for(Page pg : pages){
+			if(pg.getName().equals(name)) return pg;
+		}
+		return null;
+	}
+	
+	public Page getPage(Page match){
+		for(Page pg : pages){
+			if(pg.equals(match)) return pg;
+		}
+		return null;
+	}
+	
+	public void addDocListener(DocumentListener obj){
+		listeners.add(obj);
+	}
+	
+	private void fireChanged(String itemName, boolean removed){
+		for(DocumentListener dl : listeners){
+			dl.documentUpdated(itemName, removed);
+		}
+	}
+	
+	private void load(){
+		
+		JFileChooser choose = new JFileChooser();
+		choose.showOpenDialog(null);
+		File file = null;
+		file = choose.getSelectedFile();
+		
+		if(file != null){
+			Page[] arr = null;
+			try {
+				XMLDecoder xmlIn = new XMLDecoder(new BufferedInputStream(
+						new FileInputStream(file)));
+	
+				arr = (Page[]) xmlIn.readObject();
+				xmlIn.close();
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			
+			for(int i=0; i<arr.length; i++){
+				addPage(arr[i]);
+			}
+			
+			String base = file.getAbsolutePath();
+			int indexofslash = base.lastIndexOf(file.separator);
+			base = base.substring(0, indexofslash);
+			File updated = new File(base);
+			//rc.load(updated);
+		}
+	}
+	
+	private void load(String path){
+		File file = new File(path);
+		Page[] arr = null;
+		try {
+			XMLDecoder xmlIn = new XMLDecoder(new BufferedInputStream(
+					new FileInputStream(file)));
+
+			arr = (Page[]) xmlIn.readObject();
+			xmlIn.close();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		for(int i=0; i<arr.length; i++){
+			pages.add(arr[i]);
+		}
+		
+		String base = file.getAbsolutePath();
+		int indexofslash = base.lastIndexOf(file.separator);
+		base = base.substring(0, indexofslash);
+		File updated = new File(base);
+		//rc.load(updated);
+	}
+	
+	/**
+	 * Dumps XML page/shapemodel data to file then calls a save on the resources collection
+	 * The file passed to RC is the directory where resources should be saved directly to
+	 */
+	public void save(){
+		
+		JFileChooser choose = new JFileChooser();
+		choose.showSaveDialog(null);
+		File file = choose.getSelectedFile();
+		file.mkdir();
+		String path = file.getAbsolutePath();
+		
+		
+		Page[] arr = new Page[pages.size()];
+		for(int i=0; i<pages.size(); i++){
+			arr[i] = pages.get(i);
+		}
+		
+		try {
+			XMLEncoder xmlOut = new XMLEncoder(
+					new BufferedOutputStream(
+							new FileOutputStream(path+file.separator+"index.xml")));
+
+			xmlOut.writeObject(arr);
+			xmlOut.close();
+		}
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		//rc.save(file);
+	}
+}
